2e04d51fe424155de6e5bcfc15d4e390b8498f0f
Herik Trombetta Rodrigues <herikrodr@hotmail.com>
2024-11-06 11:26:33 -0300

    correcao bugs

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const statisticsRoutes = require('./routes/statisticsRoutes');
 const playersRoutes = require('./routes/playersRoutes'); 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; // Usa a porta do Render ou a 5000 como fallback
 
 app.use(cors());
 app.use(express.json()); 
--- backend/services/playersService.js
+++ backend/services/playersService.js
@@ -5,7 +5,13 @@ const API_URL = 'https://www.sofascore.com/api/v1/team/7315/players';
 const fetchPlayers = async () => {
 
   try {
-    const response = await axios.get(API_URL);
+    const response = await axios.get('API_URL', {
+      headers: {
+        'User-Agent': 'axios/1.7.7',  // Ou qualquer outro User-Agent que funcione no seu localhost
+        'Accept': 'application/json',
+      }
+    });
+    
 
     const players = response.data.players; 
 
@@ -20,8 +26,8 @@ const fetchPlayers = async () => {
     });
     
   } catch (error) {
-    console.error("Erro ao buscar os jogadores do Avai:", error);
-    throw new Error("Erro ao buscar os jogadores do Avai");
+    console.error("Erro ao buscar os jogadores do Avai......:", error);
+    throw new Error("Erro ao buscar os jogadores do Avai......");
   }
 };
 
