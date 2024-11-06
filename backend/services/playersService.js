75906bc17efeeb3e2c3f5880e8f19d2c01478a1f
Herik Trombetta Rodrigues <herikrodr@hotmail.com>
2024-11-06 12:09:27 -0300

    correcao bugs

--- backend/services/playersService.js
+++ backend/services/playersService.js
@@ -7,8 +7,7 @@ const fetchPlayers = async () => {
   try {
     const response = await axios.get(API_URL, {
       headers: {
-        'User-Agent': 'axios/1.7.7',
-        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
       }
     });
     
